baf2fa7cf337bf466ecae80adcd180448bf133a2
SarthakKotewale <sarthakk0808@gmail.com>
2024-07-30 12:08:16 -0700

    changes in index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT;
 
 const connectDB = require('./connect')
 //connected to mongo
 connectDB();
 
-//Routers
-const adminRouter = require('./routes/admin')
-const productRouter = require('./routes/product') 
-const userRouter = require('./routes/user')
-const categoryRouter = require('./routes/category')
-
-const PORT = process.env.PORT;
 
 require("./models/userDetail");
 
@@ -21,10 +15,31 @@ app.use(express.json());
 // static files from uploads
 app.use('/uploads', express.static('uploads'));
 
-app.use('/user', userRouter.router)
+
+//Admin routes
+const adminRouter = require('./routes/admin')
 app.use('/admin', adminRouter.router)
+
+//Product routes
+const productRouter = require('./routes/product')
 app.use('/', productRouter.router)
+
+//User routes
+const userRouter = require('./routes/user')
+app.use('/user', userRouter.router)
+
+// Category routes
+const categoryRouter = require('./routes/category')
 app.use('/categories', categoryRouter.router)
+
+
+
+
+
+
+
+
+
 
 
 app.listen(PORT, () => {
